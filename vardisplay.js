814f8099a0a651fb93bc737b7cbd349f5fde31ac
Kjartan Hrafnkelsson <imcanada@imcanada.cc>
2020-12-10 16:23:45 -0800

    Add files via upload

--- vardisplay.js
+++ vardisplay.js
@@ -19,15 +19,54 @@ let vardisplay = {
             }
             if (!xssmatch) {
                 const escapedMatch = matchedString.replace(/([{}<>/\\\\=;$(),]*)/g, '');
-                const keyValue = eval(escapedMatch);
+                
                 if (vardisplay.debug) {
                     vardisplay.log(`Escaped match: ${escapedMatch}`);
                     vardisplay.log(`Key value: ${keyValue}`);
                 }
-                html.innerHTML = html.innerHTML.replace(matchedString, keyValue);
+                try {
+                    const keyValue = eval(escapedMatch);
+                    html.innerHTML = html.innerHTML.replace(matchedString, keyValue);
+                }
+                catch (err) {
+                    vardisplay.log(`Could not display key value for "${escapedMatch}".`);
+                }
             }
         }
         return "OK";
+    },
+    render: (keyword) => {
+        const html = document.getElementsByTagName('html')[0];
+        const regex = new RegExp(`{{${keyword}}}`, 'g');
+        const match = html.innerHTML.match(regex);
+        if (!match || match.length == 0) {
+            return false;
+        }
+        for (let c = 0; c < match.length; c++) {
+            const matchedString = match[c];
+            const xssmatch = matchedString.match(/([\<\>\=\;\$\(\)\,\/])/g);
+            if (vardisplay.debug) {
+                vardisplay.log(`Matched string: ${matchedString}`);
+                vardisplay.log(`Matched XSS: ${xssmatch}`);
+            }
+            if (!xssmatch) {
+                const escapedMatch = matchedString.replace(/([{}<>/\\\\=;$(),]*)/g, '');
+                
+                if (vardisplay.debug) {
+                    vardisplay.log(`Escaped match: ${escapedMatch}`);
+                    vardisplay.log(`Key value: ${keyValue}`);
+                }
+                try {
+                    const keyValue = eval(escapedMatch);
+                    html.innerHTML = html.innerHTML.replace(matchedString, keyValue);
+                    return true;
+                }
+                catch (err) {
+                    vardisplay.log(`Could not display key value for "${escapedMatch}".`);
+                    return false;
+                }
+            }
+        }
     }
 }
 
